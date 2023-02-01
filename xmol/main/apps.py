be18c6e72afd84aceabde113e323471b47733942
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-01 13:51:46 +0100

    Fix config name

--- xmol/main/apps.py
+++ xmol/main/apps.py
@@ -17,7 +17,7 @@ class XaiMethod:
     explainer: ExplainerBase
 
 
-class XaiGnnConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
 

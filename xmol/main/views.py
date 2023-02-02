c60c6527b45f26484de950815f93ba2039606917
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-02 13:47:28 +0100

    Handle wrong or missing smiles

--- xmol/main/views.py
+++ xmol/main/views.py
@@ -1,27 +1,38 @@
-from django.http import HttpResponse
+import base64
+import tempfile
+from typing import Dict
+
 from django.shortcuts import render
 
 from .apps import MainConfig as conf
 from .ml.explanations import *
+from torch_geometric.data import Data
 
-import base64
 
-import tempfile
+def get_explanations(data: Data) -> Dict[str, str]:
+    prediction = int(conf.gcn_model.predict(data.x, data.edge_index))
+    explanations = {}
 
+    for xai_method in conf.xai_methods:
+        if type(xai_method.explainer) in [SubgraphX, PGExplainer]:
+            continue
+        explanation_img = explain(data, xai_method.explainer, prediction)
+        with tempfile.NamedTemporaryFile() as f:
+            explanation_img.save(f.name, format="PNG")
+            explanations[xai_method.name] = base64.b64encode(f.read()).decode()
+
+    return explanations
 
 def index(request):
     if request.method == 'GET':
         smiles = request.GET.get('smiles')
-        data = data_from_smiles(smiles)
-        prediction = int(conf.gcn_model.predict(data.x, data.edge_index))
-        response = {}
 
-        for xai_method in conf.xai_methods:
-            if type(xai_method.explainer) in [SubgraphX, PGExplainer]:
-                continue
-            explanation_img = explain(data, xai_method.explainer, prediction)
-            with tempfile.NamedTemporaryFile() as f:
-                explanation_img.save(f.name, format="PNG")
-                response[xai_method.name] = base64.b64encode(f.read()).decode()
+        if not smiles:
+            return render(request, "index.html")
 
-        return render(request, "index.html", context=response)
+        try:
+            data = data_from_smiles(smiles)
+        except AttributeError:
+            return render(request, "index.html", context={"error": True})
+
+        return render(request, "index.html", context=get_explanations(data))

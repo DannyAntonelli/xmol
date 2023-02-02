18649ed61cce882a77e79fa1f3c3b0d5cb310a6f
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-02 14:08:53 +0100

    Add About page

--- xmol/main/urls.py
+++ xmol/main/urls.py
@@ -6,8 +6,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path(
-        '/xai_methods',
+        'xai_methods',
         TemplateView.as_view(template_name='xai_methods.html'),
         name='xai_methods',
+    ),
+    path(
+        'about',
+        TemplateView.as_view(template_name='about.html'),
+        name='about',
     ),
 ]

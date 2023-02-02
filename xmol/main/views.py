ff8e5571e1b38742e87d92fb3ffc653b5e88ac2a
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-02 11:22:19 +0100

    Add search page

--- xmol/main/views.py
+++ xmol/main/views.py
@@ -1,5 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .apps import MainConfig as conf
+from .ml.explanations import *
+
+import base64
+
+import tempfile
 
 
 def index(request):
-    return HttpResponse()
+    if request.method == 'GET':
+        smiles = request.GET.get('smiles')
+        data = data_from_smiles(smiles)
+        prediction = int(conf.gcn_model.predict(data.x, data.edge_index))
+        response = {}
+
+        for xai_method in conf.xai_methods:
+            if type(xai_method.explainer) in [SubgraphX, PGExplainer]:
+                continue
+            explanation_img = explain(data, xai_method.explainer, prediction)
+            with tempfile.NamedTemporaryFile() as f:
+                explanation_img.save(f.name, format="PNG")
+                response[xai_method.name] = base64.b64encode(f.read()).decode()
+
+        return render(request, "index.html", context=response)

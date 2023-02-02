d12139a001c6f93cd2283317b310cab433770255
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-02 14:06:55 +0100

    Add Xai Methods page

--- xmol/main/urls.py
+++ xmol/main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path(
+        '/xai_methods',
+        TemplateView.as_view(template_name='xai_methods.html'),
+        name='xai_methods',
+    ),
 ]

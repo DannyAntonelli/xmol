da7194af63e84d557a0d103f94406f91d45ab115
dannyantonelli <dannyantonelli2002@gmail.com>
2023-02-15 10:57:07 +0100

    Add Docker container for letencrypt

--- xmol/xmol/settings.py
+++ xmol/xmol/settings.py
@@ -29,6 +29,8 @@ DEBUG = int(os.environ.get('DEBUG', 0))
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 
 # Application definition
 
